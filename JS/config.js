3b002fa83aa924e2dd49c294cbe1c6dbae650a91
HarryFozzy <119067404+HarryFozzy@users.noreply.github.com>
2023-03-09 09:30:29 +0000

    NewBackgroundDividers
    Changes the curve
    Made it responsive
    Changed the colours
    Corrected the hover
    Changed the timeline to work as expected

---
+++ JS/config.js
@@ -0,0 +1,13 @@
+// Update these values to match those of your Server URL, DriveWorks Group Alias
+// and Project Name that you wish to be rendered.
+
+const config = {
+    serverUrl: "http://wsm-tech-harrf:8020",
+    groupAlias: "FunWith3D",
+    projectName: "RubixCube",
+    // (Optional) Set Specification ping interval - in seconds
+    // A Specification will timeout after a configured period of inactivity (see DriveWorksConfigUser.xml).
+    // This function prevents a Specification timing out as long as the page is in view.
+    // Disable the ping by changing the setting to 0
+    specificationPingInterval: 0,
+};
